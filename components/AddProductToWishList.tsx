8cb060fd7230e1617152920d20187baea32263e9
thiagohrcosta <thiagohrcosta86@gmail.com>
2022-03-17 16:36:56 -0300

    adding lazy loading

---
+++ components/AddProductToWishList.tsx
@@ -0,0 +1,17 @@
+interface AddProductToWishListProps {
+  onAddToWishList: () => void;
+  onRequestClose: () => void;
+}
+
+export default function AddProductToWishList({
+  onAddToWishList,
+  onRequestClose
+}: AddProductToWishListProps) {
+  return (
+    <span>
+      Deseja adicionar aos favoritos?
+      <button onClick={onAddToWishList}>Sim</button>
+      <button onClick={onRequestClose}>Não</button>
+    </span>
+  )
+}
